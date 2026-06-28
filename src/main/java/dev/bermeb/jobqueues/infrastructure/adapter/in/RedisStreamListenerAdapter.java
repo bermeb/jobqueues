a450717c7660837e8fb9330e15149706f2c00e8e
Bernhard <bernhard@bermeb.dev>
2026-06-28 16:15:13 +0200

    add redis config with custom stream listener

---
+++ src/main/java/dev/bermeb/jobqueues/infrastructure/adapter/in/RedisStreamListenerAdapter.java
@@ -0,0 +1,43 @@
+package dev.bermeb.jobqueues.infrastructure.adapter.in;
+
+import dev.bermeb.jobqueues.domain.model.Job;
+import dev.bermeb.jobqueues.domain.port.in.ProcessJob;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.connection.stream.MapRecord;
+import org.springframework.data.redis.stream.StreamListener;
+import org.springframework.stereotype.Component;
+import tools.jackson.core.JacksonException;
+import tools.jackson.databind.json.JsonMapper;
+
+@Slf4j
+@Component
+public class RedisStreamListenerAdapter implements StreamListener<String, MapRecord<String, String, String>> {
+
+    private final ProcessJob processJob;
+    private final JsonMapper jsonMapper;
+
+    public RedisStreamListenerAdapter(ProcessJob processJob, JsonMapper jsonMapper) {
+        this.processJob = processJob;
+        this.jsonMapper = jsonMapper;
+    }
+
+    @Override
+    public void onMessage(MapRecord<String, String, String> record) {
+        String messageId = record.getId().getValue();
+        String payload = record.getValue().get("payload");
+
+        if (payload == null) {
+            log.error("Message {} has no payload. Skipping.", messageId);
+            return;
+        }
+
+        try {
+            Job job = jsonMapper.readValue(payload, Job.class);
+
+            processJob.process(job, messageId);
+        } catch (JacksonException e) {
+            log.error("Error while deserialising message {}.", messageId, e);
+        }
+     }
+}
--- src/main/java/dev/bermeb/jobqueues/infrastructure/config/RedisConfig.java
+++ src/main/java/dev/bermeb/jobqueues/infrastructure/config/RedisConfig.java
@@ -1,12 +1,93 @@
 package dev.bermeb.jobqueues.infrastructure.config;
 
+import dev.bermeb.jobqueues.infrastructure.adapter.in.RedisStreamListenerAdapter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.RedisSystemException;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.stream.Consumer;
+import org.springframework.data.redis.connection.stream.MapRecord;
+import org.springframework.data.redis.connection.stream.ReadOffset;
+import org.springframework.data.redis.connection.stream.StreamOffset;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.stream.StreamMessageListenerContainer;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.time.Duration;
+import java.util.concurrent.Executors;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${app.queue.stream-name:jobs:stream}")
     private String streamName;
+    @Value("${app.queue.group-name:jobs:group}")
     private String groupName;
+    @Value("${app.queue.consumer-name:worker-0}")
     private String consumerName;
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringRedisSerializer);
+        template.setValueSerializer(stringRedisSerializer);
+        template.setHashKeySerializer(stringRedisSerializer);
+        template.setHashValueSerializer(stringRedisSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public JsonMapper jsonMapper() {
+        return JsonMapper.builder().build();
+    }
+
+    @Bean
+    public StreamMessageListenerContainer<String, MapRecord<String, String, String>> streamMessageListenerContainer(
+        RedisConnectionFactory connectionFactory,
+        RedisStreamListenerAdapter adapter
+    ) {
+        initializeStreamAndGroup(connectionFactory);
+
+        var options = StreamMessageListenerContainer.StreamMessageListenerContainerOptions.builder()
+                .batchSize(10)
+                .pollTimeout(Duration.ofSeconds(2))
+                .executor(Executors.newVirtualThreadPerTaskExecutor())
+                .build();
+
+        var container = StreamMessageListenerContainer.create(connectionFactory, options);
+
+        container.receive(
+                Consumer.from(groupName, consumerName),
+                StreamOffset.create(streamName, ReadOffset.lastConsumed()),
+                adapter
+        );
+
+        container.start();
+        return container;
+    }
+
+    private void initializeStreamAndGroup(RedisConnectionFactory connectionFactory) {
+        try (var conn = connectionFactory.getConnection()) {
+            conn.streamCommands().xGroupCreate(
+                    streamName.getBytes(),
+                    groupName,
+                    ReadOffset.from("0"),
+                    true
+            );
+        } catch (RedisSystemException e) {
+            if (e.getRootCause() != null) {
+                if (!e.getRootCause().getMessage().contains("BUSYGROUP")) {
+                    throw e;
+                }
+            }
+        }
+    }
 
 }
